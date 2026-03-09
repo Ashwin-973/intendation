804336f97b7d5cb1d0ee647b634bc7e73243a74b
Tony Montana <ashwin.s1336@gmail.com>
2026-03-09 15:50:02 +0530

    feat : improve error handling

--- zion_mainframe/src/main.py
+++ zion_mainframe/src/main.py
@@ -1,17 +1,22 @@
 from src.matrix import extract_operator_signature
 
 import asyncio
+from colorama import init, Fore, Back, Style
+import traceback
+import logging
 
+
+init(autoreset=True) #initialize colorama
 
 async def run_uplink():
     try:
         async with asyncio.TaskGroup() as tg:
-            tg.create_task(extract_operator_signature("Trinity",3000))
-            tg.create_task(extract_operator_signature("Neo",5000))
-            tg.create_task(extract_operator_signature("Morpheus",7000))
-            tg.create_task(extract_operator_signature("Mouse",2000))
+            tg.create_task(extract_operator_signature("Trinity",300))
+            tg.create_task(extract_operator_signature("Neo",500))
+            # tg.create_task(extract_operator_signature("Morpheus",7000))
+            tg.create_task(extract_operator_signature("Mouse",200))
     except ExceptionGroup as eg:
-        print(f"an exception occured in one or more of the extract operation")
+        print(Fore.RED + f"an exception occured in one or more of the extract operation")
         raise eg
 
 
@@ -21,7 +26,10 @@ if __name__=="__main__":
     try:
         asyncio.run(run_uplink())
     except Exception as e:
-        print(f"Error at Main Program : {e}")
+        traceback.print_exc()
+        # Capture the traceback as a string for logging
+        error_msg = traceback.format_exc()
+        logging.error("Detailed error info:\n%s", error_msg) 
 
 
 '''I don't like the idea that I'm not in control of my life --Neo'''
--- zion_mainframe/src/matrix/uplink.py
+++ zion_mainframe/src/matrix/uplink.py
@@ -1,13 +1,15 @@
 
 import asyncio
+from colorama import init, Fore, Back, Style
 
+init(autoreset=True)
 
 async def extract_operator_signature(name:str,filesize:int)->int:
     try:
         print(f"[TANK] extracting signature of {name}....")
         if name=="Morpheus":
-            raise ConnectionError("Agent Smith intercepted Morpheus' signal , sever the line immediately to avoid him tracking back zion's ship")
+            raise ConnectionError(Back.red+"Agent Smith intercepted Morpheus' signal , sever the line immediately to avoid him tracking back zion's ship")
         await asyncio.sleep(filesize/100)
-        print(f"[TANK] finished extracting signature of {name}!!")
+        print(Fore.Green+f"[TANK] finished extracting signature of {name}!!")
     except Exception as e:
         raise e
