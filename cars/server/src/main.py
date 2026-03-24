b74ef366498d47b41a91263cfff22ba81347eefe
Tony Montana <ashwin.s1336@gmail.com>
2026-03-24 17:37:52 +0530

    feat : ground work done

---
+++ cars/server/src/main.py
@@ -0,0 +1,51 @@
+import uvicorn
+import asyncio
+from fastapi import FastAPI,HTTPException,Depends
+from contexlib import asynccontextmanager
+
+app=FastAPI(title="Shelby American",
+            version="1.0.0",
+            docs_url="/docs",
+            redoc_url="/redoc")
+
+@asynccontextmanager
+async def cars_lifespan():
+    print("[MAIN] Loading Heavy Car Model")
+
+    await asyncio.sleep(2)
+
+    HEAVY_CAR_MODEL:str="LOADED"
+
+    app.state.car_model=HEAVY_CAR_MODEL
+
+    yield None
+
+    print("[MAIN] Unloading Heavy Car Model")
+
+    HEAVY_CAR_MODEL:str="UNLOADED"
+
+
+@app.get("/")
+def screening():
+    return {"success":True,"message":"Welcome to Shelby American . Get Ready to dive into the world of fast,sexy and exotic cars . Buckle Up!!"}
+
+
+@app.get()
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    uvicorn.run(app,host="127.0.0.1",port=6000,reload=True)
