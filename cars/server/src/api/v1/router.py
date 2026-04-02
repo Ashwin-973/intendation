3180ed782a3804468d262d793cbbb96477037932
Tony Montana <ashwin.s1336@gmail.com>
2026-04-02 16:41:57 +0530

    feat : add all route handlers + business logic

--- cars/server/src/api/v1/router.py
+++ cars/server/src/api/v1/router.py
@@ -43,7 +43,8 @@ def delete_car(car_id: int, db: Dict[int, Car] = Depends(get_db)):
 
 
 
-from fastapi import APIRouter,HTTPException,Depends
+from fastapi import APIRouter,HTTPException,Depends,Query
+from typing import Annotated
 
 
 from src.schemas import Car
@@ -53,47 +54,72 @@ from src.db import get_db
 
 router=APIRouter()
 
-@router.get("/cars/{id}",response_model=Car)
-def get_car(id:int,db:list[dict]=Depends(get_db)):
+@router.get("/cars/{id}",response_model=Car|dict[str,bool|str])
+def get_car(id:int,db:list[Car]=Depends(get_db)):
     print("[ROUTER] GET /cars/id")
-    return db[id]
+    retrieved_car=None
+    for car_data in db: #!prevented variable shadowing
+        if car_data.id==id:
+            retrieved_car=car_data
+            return retrieved_car 
+    return {"success":False,"message":"car object with the given id does not exist"}  
 
 @router.get("/cars",response_model=list[Car])
 def get_all_cars(db:list[Car]=Depends(get_db)):
     print("[ROUTER] GET /cars")
+    print(db)
     return db
 
 @router.post("/cars",response_model=Car)
 def create_car(car:Car,db:list[Car]=Depends(get_db)):
     print("[ROUTER] POST /cars")
     db.append(car)
-    return db[len(db) - 1]
+    print(db)
+    return db[len(db) - 1]  
 
 
 @router.put("/cars/{id}",response_model=Car)
 def update_car(id:int,car:Car,db:list[Car]=Depends(get_db)):
     print("[ROUTER] PUT /cars")
-    print(id)
-    db[id]=car
-    return db[id]
+
+    for idx,car_data in enumerate(db):
+        if car_data.id==id:
+            db[idx]=car
+            return db[idx]
+    return {"success":False,"message":"car object with the given id does not exist"}  
 
 @router.patch("/cars/{id}",response_model=Car)
-def patch_car(id:int,name:str|None,brand:str|None,price:str|None,image_url:str|None,db:list[Car]=Depends(get_db)):
+def patch_car(id:int,name:Annotated[str|None,Query(min_length=3)]=None,brand:str|None=None,price:str|None=None,image_url:str|None=None,db:list[Car]=Depends(get_db)):
+    print("[ROUTER] PATCH /cars")
     if not (name or brand or price or image_url):
         raise HTTPException(400,detail="nothing to patch. incomplete info")
     
-    new_car={
-        "id":db[id]["id"],
-        "name":name | db[id]["name"],
-        "brand":brand | db[id]["brand"],
-        "price":price | db[id]["price"],
-        "image_url":image_url | db[id]["price"]
-    }
+    for idx,car_data in enumerate(db):
+        if car_data.id==id:
+            updated_car=Car(
+                id=car_data.id,
+                name=name or car_data.name,
+                brand=brand or car_data.brand,
+                price=price or car_data.price,
+                image_url=image_url or car_data.image_url,
+            )
+            db[idx]=updated_car
+            return db[idx]
 
-    db[id]=new_car
-
-    return db[id]
+    return {"success":False,"message":"car object with the given id does not exist"}  
 
 
+@router.delete("/cars/{id}",response_model=dict[str,bool|str])
+def delete_car(id:int,db:list[Car]=Depends(get_db)):
+    print("[ROUTER] DELETE /cars")
+    
+    for idx,car_data in enumerate(db):
+        if car_data.id==id:
+            del db[idx]
+            return {"success":True,"message":"car has been deleted"}  
+    return {"success":False,"message":"car object with the given id does not exist"}  
+    
+
+    
     
 
--- cars/server/src/db/session.py
+++ cars/server/src/db/session.py
@@ -1,7 +1,10 @@
 
 from data import CAR_DB
+from src.schemas import Car
+
+db=[Car(**car) for car in CAR_DB]
 
 def get_db():
     print("[DEPENEDNCIES] providing a route access to db")
-    yield CAR_DB
+    yield db
     print("[DEPENEDNCIES] route handler finished execution")
